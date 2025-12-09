ac2d4b6cd6b2a715ddc801a4fb8fea9c6d75a373
Mohammed Musaddiq <121399233+Musaddiq9945@users.noreply.github.com>
2025-12-10 00:16:59 +0530

    Update Registration Number OCR.py

--- Registration Number OCR.py
+++ Registration Number OCR.py
@@ -1,5 +1,6 @@
 import easyocr
 
 reader = easyocr.Reader(['en'],gpu= True)
-results = reader.readtext("C:/Users/moham/Downloads/images.jpeg")
+results = reader.readtext("C:/Users/moham/Downloads/images.jpeg") # Replace with your cropped images' path 
+
 print(f"Detected Registration Number: {results}")
