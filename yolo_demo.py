0ffbc52d4cff6a5a93acda9a5ef61cba9bf98e1a
Mohammed Musaddiq <121399233+Musaddiq9945@users.noreply.github.com>
2025-12-10 00:09:37 +0530

    Update yolo_demo.py

--- yolo_demo.py
+++ yolo_demo.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO("C:/Users/moham/Downloads/best.pt")
+model = YOLO("C:/Users/moham/Downloads/best.pt") # Replace with your model"s download path
 
 results = model(source=0 ,device = "cuda:0", show=True, save_crop = True)
+
 
